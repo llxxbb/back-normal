23c26512629da927fdc48da2f86681708f737ac2
lixuebin <lixuebin@cdeledu.com>
2023-06-16 16:33:57 +0800

    add pinpoint config property

--- src/tool/pinpoint.go
+++ src/tool/pinpoint.go
@@ -8,18 +8,26 @@ import (
 type PinPointConfig struct {
 	ServerURL   string
 	CounterRate int
+	AgentPort   int
+	StatPort    int
+	SpanPort    int
 }
 
 func (c *PinPointConfig) AppendFieldMap(fMap map[string]string) {
 	fMap["pinpoint.url"] = "PinPoint.ServerURL"
 	fMap["pinpoint.sampling.counterRate"] = "PinPoint.CounterRate"
+	fMap["pinpoint.agentPort"] = "PinPoint.AgentPort"
+	fMap["pinpoint.statPort"] = "PinPoint.StatPort"
+	fMap["pinpoint.spanPort"] = "PinPoint.SpanPort"
 }
 
 func (c *PinPointConfig) Print() {
 	zap.L().Info("------------ pinpoint ------------")
-	zap.L().Info("-- ", zap.String("url", c.ServerURL))
+	zap.L().Info("-- ", zap.String("collector.url", c.ServerURL))
+	zap.L().Info("-- ", zap.Int("collector.agentPort", c.AgentPort))
+	zap.L().Info("-- ", zap.Int("collector.statPort", c.StatPort))
+	zap.L().Info("-- ", zap.Int("collector.spanPort", c.SpanPort))
 	zap.L().Info("-- ", zap.Int("sampling.counterRate", c.CounterRate))
-
 }
 
 func (c *PinPointConfig) InitPinPoint(prj string, host string) pinpoint.Agent {
@@ -27,6 +35,9 @@ func (c *PinPointConfig) InitPinPoint(prj string, host string) pinpoint.Agent {
 		pinpoint.WithAppName(prj),
 		pinpoint.WithAgentId(host),
 		pinpoint.WithCollectorHost(c.ServerURL),
+		pinpoint.WithCollectorAgentPort(c.AgentPort),
+		pinpoint.WithCollectorStatPort(c.StatPort),
+		pinpoint.WithCollectorSpanPort(c.SpanPort),
 		pinpoint.WithSamplingCounterRate(c.CounterRate),
 		pinpoint.WithLogLevel("Error"),
 	}
