6cfdd37875e72f121f80fc6bf8d21a0057c365f8
lixuebin <lixuebin@cdeledu.com>
2023-09-12 16:03:56 +0800

    test: inheritance json

--- src/internal/service/demo/inheritance/inheritance_test.go
+++ src/internal/service/demo/inheritance/inheritance_test.go
@@ -1,6 +1,7 @@
 package inheritance
 
 import (
+	json2 "github.com/goccy/go-json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,4 +12,12 @@ func TestInheritance(t *testing.T) {
 	// 等同于 animal.Mammal.Greet()，Mammal 为匿名字段，可省略不写。
 	word := animal.Greet()
 	assert.Equal(t, "Hello, I'm Bob", word)
+}
+
+func TestJson(t *testing.T) {
+	animal := Animal{Mammal{"Bob", 10}}
+	// 等同于 animal.Mammal.Greet()，Mammal 为匿名字段，可省略不写。
+	marshal, _ := json2.Marshal(animal)
+	json := string(marshal)
+	assert.Equal(t, "{\"Name\":\"Bob\",\"Age\":10}", json)
 }
